39da2cdcbc4d5e5d25b1e6b7941bee59055eb4e5
sourcecorearch <sourcecorearch@gmail.com>
2026-02-26 17:33:41 -0300

    para test en iso live

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
-pub const APP_ID: &str = "org.eoslinux.welcome";
-pub const PROFILE: &str = "";
-pub const VERSION: &str = "0.1.0";
+pub const APP_ID: &str = "org.eoslinux.welcome.Devel";
+pub const PROFILE: &str = "Devel";
+pub const VERSION: &str = "0.1.0-bf0afd3
+";
